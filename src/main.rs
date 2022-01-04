ec9ee9c52264565ac21103bc061016c9c49ec4ca
Liss Heidrich <Clueliss@users.noreply.github.com>
2022-01-05 00:31:59 +0100

    added commandline interface descriptions

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,37 @@
 mod gdbus;
 mod session;
 
-use std::borrow::Cow;
-
-use clap::{AppSettings, Clap};
+use clap::{AppSettings, Clap, crate_authors, crate_version, crate_description};
 use gdbus::GnomeShellDBusProxy;
 use zbus::Connection;
 
 #[derive(Debug, Clap)]
 enum SessionAction {
+    #[clap(about = "Saves the current gnome session")]
     Save,
+    #[clap(about = "Restores a gnome session from disk")]
     Restore {
-        #[clap(long)]
+        #[clap(long, about = "Removes the session file after restoring")]
         rm: bool,
 
-        #[clap(long)]
+        #[clap(long, about = "Marks the session file with the current timestamp after restoring")]
         mark: bool,
     },
 }
 
 #[derive(Debug, Clap)]
-#[clap(setting = AppSettings::SubcommandRequired)]
+#[clap(setting = AppSettings::SubcommandRequired, version = crate_version!(), author = crate_authors!(), about = crate_description!())]
 struct Opts {
-    #[clap(short, long)]
-    file: Option<String>,
+    #[clap(short, long, about = "Manually specify a session file", default_value = "~/.last_session")]
+    file: String,
 
-    #[clap(long, conflicts_with_all = &["session", "system"])]
+    #[clap(long, conflicts_with_all = &["session", "system"], about = "Connect to the specified D-Bus address")]
     dbus_address: Option<String>,
 
-    #[clap(long, conflicts_with = "system")]
+    #[clap(long, conflicts_with = "system", about = "Connect to the session D-Bus [default]")]
     session: bool,
 
-    #[clap(long, conflicts_with = "session")]
+    #[clap(long, conflicts_with = "session", about = "Connect to the system D-Bus")]
     system: bool,
 
     #[clap(subcommand)]
@@ -51,10 +51,7 @@ fn main() {
 
     let gdb = GnomeShellDBusProxy::new(&conn).expect("failed to create proxy");
 
-    let path = match opts.file {
-        Some(f) => Cow::Owned(f),
-        None => shellexpand::tilde("~/.last_session"),
-    };
+    let path = shellexpand::tilde(&opts.file);
 
     match opts.subcommand {
         SessionAction::Save => session::save_session(&gdb, path.as_ref()),
